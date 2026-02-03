b5f081215d1176a00e9a268b028ca4dc2420a500
Ada Öztürk <adaozturk@icloud.com>
2026-02-03 22:44:37 +0300

    Add edit coupon functionality and fix create cupon ui

--- Glowify/Areas/Admin/Controllers/CouponController.cs
+++ Glowify/Areas/Admin/Controllers/CouponController.cs
@@ -87,5 +87,45 @@ namespace Glowify.Areas.Admin.Controllers
             TempData["Success"] = "Coupon deleted successfully!";
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["Error"] = "There is no coupon with this id.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var couponFromDb = _db.Coupons.FirstOrDefault(u => u.Id == id);
+
+            if (couponFromDb == null)
+            {
+                TempData["Error"] = "There is no coupon with this id.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(couponFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Coupon obj)
+        {
+            if (_db.Coupons.Any(u => u.Code.ToLower() == obj.Code.ToLower() && u.Id != obj.Id))
+            {
+                ModelState.AddModelError("Code", "Coupon code already exists!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+
+                TempData["Success"] = "Coupon updated successfully!";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
     }
 }
