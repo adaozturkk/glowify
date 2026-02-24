99684e52a8ee0a46683e0f0b151e50c591a19c8b
Ada Öztürk <adaozturk@icloud.com>
2026-02-24 20:03:46 +0300

    Create dashboard for admin

---
+++ Glowify/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using Glowify.Data.Repository.IRepository;
+using Glowify.Models.ViewModels;
+using Glowify.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Glowify.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class DashboardController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var vm = new DashboardVM()
+            {
+                TotalUsers = _unitOfWork.ApplicationUser.GetAll().Count(),
+                TotalOrders = _unitOfWork.OrderHeader.GetAll().Count(),
+                PendingOrders = _unitOfWork.OrderHeader.GetAll(u => 
+                    u.OrderStatus != SD.StatusPending || 
+                    u.OrderStatus != SD.StatusCancelled).Count(),
+                TotalRevenue = _unitOfWork.OrderHeader.GetAll(u => 
+                    u.OrderStatus == SD.StatusShipped || 
+                    u.OrderStatus == SD.StatusDelivered).Sum(u => u.OrderTotal)
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ Glowify/Models/ViewModels/DashboardVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Glowify.Models.ViewModels
+{
+    public class DashboardVM
+    {
+        public int TotalUsers { get; set; }
+        public int TotalOrders { get; set; }
+        public double TotalRevenue { get; set; }
+        public int PendingOrders { get; set; }
+    }
+}
